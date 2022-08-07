4c9d3644e28dedfa973d48f39bbfb4162933b955
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-08-08 00:43:59 +0900

    [D2] Title: Base64 Decoder, Time: 245 ms, Memory: 67,180 KB -BaekjoonHub

---
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.py
@@ -0,0 +1,30 @@
+# 1928. Base64 Decoder
+
+import string
+
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    base64 = input()
+    full_bin = ''
+    string = ''
+    for text in base64:
+        ascii_num = ord(text)
+        if 65 <= ascii_num <= 90:
+            decoding_num = ascii_num - 65
+        elif 97 <= ascii_num <= 122:
+            decoding_num = ascii_num - 71
+        elif 48 <= ascii_num <= 57:
+            decoding_num = ascii_num + 4
+        elif ascii_num == 43:
+            decoding_num = ascii_num + 19
+        else:
+            decoding_num = ascii_num + 16
+        decoding_bin = format(decoding_num, 'b')
+        while len(decoding_bin) < 6:
+            decoding_bin = '0' + decoding_bin
+        full_bin += decoding_bin
+    for bin_index in range(8, len(full_bin), 8):
+        string += chr(int(full_bin[bin_index - 8 : bin_index], 2))
+    print(f'#{test_case} {string}.')
