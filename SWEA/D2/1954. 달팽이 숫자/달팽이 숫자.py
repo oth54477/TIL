915a2e4ed5bf891a218263e15e1422de359005f1
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-08-10 17:32:59 +0900

    [D2] Title: 달팽이 숫자, Time: 137 ms, Memory: 56,708 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,21 +1,20 @@
 def find(i, j):
     global row, col, cnt, number, snail, end_condition, N
-    while cnt < N - 1:
-        if snail[row + i][col + j] != 0:
-            break
+    if cnt >= N - 1 or snail[row + i][col + j] != 0:
+        cnt = 0
+        return None
+    else:
+        number += 1
+        if i != 0:
+            row += i
         else:
-            number += 1
-            if i != 0:
-                row += i
-            else:
-                col += j
-            snail[row][col] = number
-            end_condition = 0
-            cnt += 1
-    cnt = 0
+            col += j
+        snail[row][col] = number
+        end_condition = 0
+        cnt += 1
+        find(i, j)
 
 
-# s3.py
 T = int(input())
 for t in range(1, T + 1):
 
@@ -24,18 +23,12 @@ for t in range(1, T + 1):
     snail = [[0 for j in range(N)] for i in range(N)]  # 0으로 채워져 있는 2차원 배열 선언
     snail[0][0] = 1  # 0,0에 1 할당
     number = 1  # 할당해줄 숫자
-    row = 0
-    col = 0
-    cnt = 0
-    end_condition = 0
+    row, col, cnt, end_condition = 0, 0, 0, 0
     while number != N**2:  # number가 N의 제곱이 될 때까지 반복
-        find(0, 1)  # 오
-
-        find(1, 0)  # 아래
-
-        find(0, -1)  # 왼
-
-        find(-1, 0)  # 위
+        find(0, 1)  # 오른쪽 탐색, 채우기
+        find(1, 0)  # 아래쪽 탐색, 채우기
+        find(0, -1)  # 왼쪽 탐색, 채우기
+        find(-1, 0)  # 위쪽 탐색, 채우기
     print(f'#{t}')
     for rows in snail:
         for cols in rows:
