bec2ab960d2030e786fbc9ccd226f87c8883f2df
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-08-12 16:04:15 +0900

    [D2] Title: 새로운 버스 노선, Time: 186 ms, Memory: 62,236 KB -BaekjoonHub

---
+++ SWEA/D2/13994. 새로운 버스 노선/새로운 버스 노선.py
@@ -0,0 +1,20 @@
+for t in range(1, int(input()) + 1):
+    cnt_arr = [0] * 1001
+    n = int(input())
+    for _ in range(n):
+        bus_type, a, b = map(int, input().split())
+
+        if bus_type == 1:
+            for stop_idx in range(a, b + 1):
+                cnt_arr[stop_idx] += 1
+        elif bus_type == 2:
+            for stop_idx in range(a, b + 1, 2):
+                cnt_arr[stop_idx] += 1
+        else:
+            for stop_idx in range(a, b + 1):
+                if not a % 2 and stop_idx % 4 == 0:
+                    cnt_arr[stop_idx] += 1
+                elif a % 2 and (stop_idx % 3 == 0 and stop_idx % 10 != 0):
+                    cnt_arr[stop_idx] += 1
+
+    print(f'#{t}', max(cnt_arr))
