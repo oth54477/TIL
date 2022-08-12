64ea551cf136bd81f3838cfb4c393d092dfb0e71
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-08-12 13:54:14 +0900

    [D1] Title: 연속한 1의 개수, Time: 133 ms, Memory: 56,920 KB -BaekjoonHub

--- SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
+++ SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
@@ -1,6 +1,3 @@
-# 9386. 연속한 1의 개수
-
 for tc in range(1, int(input()) + 1):
     n = int(input())
-
-    print(f'#{tc}', len(max(list(map(str, input().split('0'))))))
+    print(f'#{tc}', len(max(input().split('0'))))
