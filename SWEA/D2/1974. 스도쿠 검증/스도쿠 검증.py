d5300e8100695f37c07c5928ca0171aad3570512
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-08-16 11:15:16 +0900

    [D2] Title: 스도쿠 검증, Time: 127 ms, Memory: 56,944 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,15 +1,10 @@
-T=int(input())
-for t in range(T):
+for t in range(int(input())):
     count = 1
-    sudoku = [[0 for j in range(9)] for i in range(9)]
-    for line in range(9):
-        sudoku[line] = input().split()
-
-        if len(set(sudoku[line])) != 9:         # 가로줄 검사
-            count = 0
+    sudoku = [list(map(int, input().split())) for _ in range(9)]
     sudoku_reverse = list(map(list, zip(*sudoku)))      # 배열 뒤집기
+
     for line in range(9):
-        if len(set(sudoku_reverse[line])) != 9:         # 세로줄 검사
+        if len(set(sudoku[line])) != 9 or len(set(sudoku_reverse[line])) != 9:         # 가로, 세로줄 검사
             count = 0
             break
     #n = 3                                           # box3개 검사할때마다 +3
@@ -23,5 +18,5 @@ for t in range(T):
             if len(box_set) != 9:
                 count = 0
                 break
-        
-    print(f'#{t+1} {count}') 
+         
+    print(f'#{t+1} {count}')
