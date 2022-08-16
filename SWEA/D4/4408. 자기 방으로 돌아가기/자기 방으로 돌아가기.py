de19ed7728cd9ac4ec7295c0b66cff1660a5d903
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-08-16 13:42:32 +0900

    [D4] Title: 자기 방으로 돌아가기, Time: 164 ms, Memory: 62,232 KB -BaekjoonHub

---
+++ SWEA/D4/4408. 자기 방으로 돌아가기/자기 방으로 돌아가기.py
@@ -0,0 +1,20 @@
+for t in range(1, int(input()) + 1):
+    n = int(input())
+    cnt = 1
+    paths = [0] * 201
+    for _ in range(n):
+        start, end = map(int, input().split())
+
+        if start % 2 == 0:
+            start_path = start // 2
+        else:
+            start_path = start // 2 + 1
+        if end % 2 == 0:
+            end_path = end // 2
+        else:
+            end_path = end // 2 + 1
+
+        for idx in range(min(start_path, end_path), max(start_path, end_path) + 1):
+            paths[idx] += 1
+
+    print(f'#{t} {max(paths)}')
