da2f3e38ad2b3338bbf385a65889a46c1d3c1fb5
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-08-17 23:27:16 +0900

    [D2] Title: 파스칼의 삼각형, Time: 130 ms, Memory: 56,708 KB -BaekjoonHub

---
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -0,0 +1,24 @@
+def pascal(cnt):
+    global arr, n
+
+    for i in range(cnt + 1):
+        if i - 1 < 0 or i + 1 > cnt:
+            arr[cnt].append(1)
+        else:
+            arr[cnt].append(arr[cnt - 1][i] + arr[cnt - 1][i - 1])
+    cnt += 1
+    if cnt == n:
+        return
+    else:
+        pascal(cnt)
+
+
+for t in range(1, int(input()) + 1):
+    n = int(input())
+    cnt = 0
+    arr = [[] for _ in range(n)]
+    pascal(cnt)
+
+    print(f'#{t}')
+    for idx in range(n):
+        print(*arr[idx])
