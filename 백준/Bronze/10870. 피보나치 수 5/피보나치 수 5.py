70fb02430ab4542b001d753b62c4c9311c374443
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-08-18 09:47:40 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,6 +1,6 @@
 def fibo(n):
-    if n <=1:
-        return n
-    return fibo(n-2) + fibo(n-1)
-
+    if len(memo) <= n:
+        memo.append(fibo(n-2) + fibo(n-1))
+    return memo[n]
+memo = [0,1]
 print(fibo(int(input())))
