0608b639a845b6066060b9c71de193bbce0b41a1
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-08-22 21:38:44 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,385 ms, Memory: 246,348 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -2,11 +2,9 @@ for t in range(1, int(input()) + 1):
     n = int(input())
     costs = list(map(int, input().split()))
     revenue = 0
-    while True:
+    while costs:
         max_cost = max(costs)
         max_idx = costs.index(max_cost)
         revenue += max_idx * max_cost - sum(costs[:max_idx])
         del costs[: max_idx + 1]
-        if not costs:
-            break
     print(f'#{t} {revenue}')
