4e4dfc12565bdb0ddc36df9654ea91c5703ee448
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-08-24 21:26:58 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 149 ms, Memory: 57,724 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -1,18 +1,14 @@
 for _ in range(10):
     t = int(input())
     queue = list(map(int, input().split()))
-
     n = 1
     while True:
         num = queue.pop(0)
         num -= n
         if num <= 0:
-            num = 0
-            queue.append(num)
+            queue.append(0)
             break
         queue.append(num)
-
         n %= 5
         n += 1
-
     print(f'#{t}', *queue)
