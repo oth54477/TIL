32c4357dbc66b2dde6e6e258ff815f4c7ab19cbf
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-08-26 11:26:31 +0900

    [D2] Title: 스도쿠 검증, Time: 135 ms, Memory: 56,684 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,22 +1,30 @@
-for t in range(int(input())):
-    count = 1
-    sudoku = [list(map(int, input().split())) for _ in range(9)]
-    sudoku_reverse = list(map(list, zip(*sudoku)))      # 배열 뒤집기
+def chk(arr):
+    if len(set(arr)) == 9:
+        return 1
+    return 0
 
-    for line in range(9):
-        if len(set(sudoku[line])) != 9 or len(set(sudoku_reverse[line])) != 9:         # 가로, 세로줄 검사
-            count = 0
-            break
-    #n = 3                                           # box3개 검사할때마다 +3
-    #m = 3                                           # box1개 검사마다 +3
-    for box in range(3,9,3):                            # box_line_1~3
-        for box_num in range(3,9,3):                    # box_1~3
-            box_set = set()
-            for row in range(box-3,box):
-                for col in range(box_num-3,box_num):
-                    box_set.add(sudoku[row][col])
-            if len(box_set) != 9:
-                count = 0
-                break
-         
-    print(f'#{t+1} {count}')
+
+for t in range(1, int(input()) + 1):
+    sudoku = [list(map(int, input().split())) for _ in range(9)]
+    sudoku_chk = 0 if 0 in set(map(chk, sudoku)) else 1
+    if not sudoku_chk:
+        print(f'#{t} {0}')
+        continue
+    sudoku_reverse = list(map(list, zip(*sudoku)))
+    reverse_chk = 0 if 0 in set(map(chk, zip(*sudoku))) else 1
+    if not reverse_chk:
+        print(f'#{t} {0}')
+        continue
+    box = []
+    for row in range(0, 7, 3):
+        for col in range(0, 7, 3):
+            box.append(
+                sudoku[row][col : col + 3]
+                + sudoku[row + 1][col : col + 3]
+                + sudoku[row + 2][col : col + 3]
+            )
+    box_chk = 0 if 0 in set(map(chk, box)) else 1
+    if not box_chk:
+        print(f'#{t} {0}')
+        continue
+    print(f'#{t} {1}')
