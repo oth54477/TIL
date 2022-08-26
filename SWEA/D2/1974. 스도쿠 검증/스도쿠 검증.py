6cffe31440925f18ad31e87b13b80339271d96af
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-08-26 11:29:18 +0900

    [D2] Title: 스도쿠 검증, Time: 137 ms, Memory: 56,940 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,3 +1,4 @@
+# 검사 함수 (집합 자료형 사용)
 def chk(arr):
     if len(set(arr)) == 9:
         return 1
@@ -5,16 +6,21 @@ def chk(arr):
 
 
 for t in range(1, int(input()) + 1):
+    # 스도쿠 입력 받기
     sudoku = [list(map(int, input().split())) for _ in range(9)]
+    # 행 검사하기
     sudoku_chk = 0 if 0 in set(map(chk, sudoku)) else 1
+    # 행 에러
     if not sudoku_chk:
         print(f'#{t} {0}')
         continue
-    sudoku_reverse = list(map(list, zip(*sudoku)))
+    # 열 검사
     reverse_chk = 0 if 0 in set(map(chk, zip(*sudoku))) else 1
+    # 열 에러
     if not reverse_chk:
         print(f'#{t} {0}')
         continue
+    # 3x3 박스 만들기
     box = []
     for row in range(0, 7, 3):
         for col in range(0, 7, 3):
@@ -23,8 +29,11 @@ for t in range(1, int(input()) + 1):
                 + sudoku[row + 1][col : col + 3]
                 + sudoku[row + 2][col : col + 3]
             )
+    # 박스 검사
     box_chk = 0 if 0 in set(map(chk, box)) else 1
+    # 박스 에러
     if not box_chk:
         print(f'#{t} {0}')
         continue
+    # 이상 없음
     print(f'#{t} {1}')
