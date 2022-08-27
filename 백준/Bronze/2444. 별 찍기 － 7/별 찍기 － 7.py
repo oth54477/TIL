c4b363d46d20b68618f9f618bda976e335ab23f3
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-08-27 22:19:12 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+ptn, space = '*', ' '
+for m in range(1, n + 1):
+    line = space * (n - m) + ptn * (2 * m - 1)
+    print(line)
+for m in range(n-1, 0, -1):
+    line = space * (n - m) + ptn * (2 * m - 1)
+    print(line)
