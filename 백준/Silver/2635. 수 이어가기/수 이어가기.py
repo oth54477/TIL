0f416b6e3fcb32f3c9aa119a1444a2bdb165c4a8
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-02 23:10:10 +0900

    [Silver V] Title: 수 이어가기, Time: 112 ms, Memory: 35956 KB -BaekjoonHub

--- 백준/Silver/2635. 수 이어가기/수 이어가기.py
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.py
@@ -6,13 +6,17 @@ for i in range(1, n + 1):
     result = []
     q = [n, i]
     while True:
+        # An = A(n-2) - A(n-1)
         q.insert(2, q[0] - q[1])
-        # print(result)
+        # n이 음수이면 A(n-1)과 A(n-2) 리스트에 더한 후정지
         if q[2] < 0:
             result += q[:2]
             break
+        # A(n-2)는 pop하여 리스트에 추가 q는 한 칸씩 앞으로
         result.append(q.pop(0))
     arr[i] = result
-ml = max(list(map(lambda x: len(x), arr)))
+# 최고 길이
+ml = max(list(map(len, arr)))
 print(ml)
-print(*arr[list(map(lambda x: len(x), arr)).index(ml)])
+# 가장 긴 케이스
+print(*arr[list(map(len, arr)).index(ml)])
