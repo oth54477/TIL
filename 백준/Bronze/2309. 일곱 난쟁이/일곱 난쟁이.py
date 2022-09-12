51528d7c195a15e951e9b08f94e5e338518eea6d
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-12 18:34:39 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,15 @@
+arr = [int(input()) for _ in range(9)]
+
+sum_n = sum(arr)
+
+num = sum_n - 100
+
+for i in arr:
+    n = num - i
+    if n in arr and n != i:
+        num1 = i
+        num2 = n
+del arr[arr.index(num1)]
+del arr[arr.index(num2)]
+
+print(*sorted(arr), sep='\n')
