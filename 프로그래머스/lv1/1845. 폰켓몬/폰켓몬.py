eb65861df1fb6d96469701b400719a11f98e9f8f
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-12 14:00:53 +0900

    [level 1] Title: 폰켓몬, Time: 0.62 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,9 +1,2 @@
 def solution(nums):
-    set_nums = set(nums)
-    num = len(set_nums)
-    n_2 = len(nums) // 2
-    if num > n_2:
-        result = n_2
-    else:
-        result = num
-    return result
+    return len(nums) // 2 if len(set(nums)) > len(nums) // 2 else len(set(nums))
