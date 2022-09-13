8b93d394cd897aae6c5fcce48c1d23d1878d6338
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-13 18:08:21 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 162 ms, Memory: 57,180 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -9,21 +9,16 @@ def inorder(n):
         if len(g) == 2:
             inorder(g[1])
 
+
 for t in range(1, 11):
     E = int(input())
     nodes = [0] * (E + 1)
     graphs = [[] for _ in range(E + 1)]
     for _ in range(E):
-        num, node, *graph = map(
-            lambda x: int(x) if x.isdigit() else str(x), input().split()
-        )
+        num, node, *graph = map(lambda x: int(x) if x.isdigit() else str(x), input().split())
         nodes[num] = node
         graphs[num] = graph
     V = E + 1
-    root = 1
-    ch1 = [0] * (V + 1)
-    ch2 = [0] * (V + 1)
-    result = ''
-
+    root, result = 1, ''
     inorder(root)
     print(f'#{t} {result}')
