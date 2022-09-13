844f86443692cf51606aa4338fc17a4487b97d11
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-13 21:11:00 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 629 ms, Memory: 61,364 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
@@ -1,12 +1,7 @@
 for t in range(1, int(input()) + 1):
     input()
     nums = {}
-    arr = map(int, input().split())
 
-    for num in arr:
-        if num in nums:
-            nums[num] += 1
-        else:
-            nums[num] = 0
-
-    print(f'#{t} {sorted(nums, key=lambda x: nums[x], reverse=True)[0]}')
+    arr = list(map(int, input().split()))
+    r = max(arr, key=arr.count)
+    print(f'#{t} {r}')
