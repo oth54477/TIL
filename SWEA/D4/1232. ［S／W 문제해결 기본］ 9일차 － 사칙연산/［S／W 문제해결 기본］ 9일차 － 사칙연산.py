5603ab5d19d8291a65194c5312f3da085e16c00d
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-15 14:15:33 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산, Time: 220 ms, Memory: 64,076 KB -BaekjoonHub

---
+++ SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.py
@@ -0,0 +1,21 @@
+cal = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x // y,
+}
+for t in range(1, 11):
+    n = int(input())
+    arrs = [
+        list(map(lambda x: int(x) if x.isdigit() else x, input().split()))
+        for _ in range(n)
+    ]
+    tree_arr = [0] * (n + 1)
+
+    for arr in arrs[::-1]:
+        if len(arr) == 2:
+            tree_arr[arr[0]] = arr[1]
+        else:
+            tree_arr[arr[0]] = cal[arr[1]](tree_arr[arr[2]], tree_arr[arr[3]])
+
+    print(f'#{t} {tree_arr[1]}')
