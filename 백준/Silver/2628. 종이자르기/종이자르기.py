0846b96b7e77d9885dd929f6fa4fcf288cb26acc
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-20 00:12:07 +0900

    [Silver V] Title: 종이자르기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -1,30 +1,31 @@
-c, r = map(int, input().split())
-
+x, y = map(int, input().split())
 n = int(input())
 
-row_arr = [i for i in range(r)]
-col_arr = [i for i in range(c)]
-# rows, cols, r_cnt, c_cnt = [], [], 0, 0
-rows, cols, r_cnt, c_cnt = [0], [0], 0, 0
+x_axis = [0]
+y_axis = [0]
+
 for _ in range(n):
-    cut, idx = map(int, input().split())
-    if cut == 0:
-        rows.append(idx)
+    d, v = map(int, input().split())
+    if d:
+        x_axis.append(v)
     else:
-        cols.append(idx)
+        y_axis.append(v)
 
-rows.sort()
-cols.sort()
+x_axis.append(x)
+y_axis.append(y)
 
-rows.append(r)
-cols.append(c)
-max_r, max_c = 0, 0
-for rr in range(1, len(rows)):
-    rrr = rows[rr] - rows[rr - 1]
-    if max_r < rrr:
-        max_r = rrr
-for cc in range(1, len(cols)):
-    ccc = cols[cc] - cols[cc - 1]
-    if max_c < ccc:
-        max_c = ccc
-print(max_c * max_r)
+x_axis.sort()
+y_axis.sort()
+
+x_value, y_value = [], []
+for idx, x in enumerate(x_axis[:-1]):
+    x_value.append(x_axis[idx + 1] - x)
+for idx, y in enumerate(y_axis[:-1]):
+    y_value.append(y_axis[idx + 1] - y)
+
+result = []
+for x in x_value:
+    for y in y_value:
+        result.append(x * y)
+
+print(max(result))
