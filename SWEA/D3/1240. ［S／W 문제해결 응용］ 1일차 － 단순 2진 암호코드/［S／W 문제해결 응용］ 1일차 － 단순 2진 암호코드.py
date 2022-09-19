ce602423a05db8e4fedd8a3d929124d6e0e6b54b
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-19 15:22:33 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 153 ms, Memory: 58,760 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
@@ -1,26 +1,13 @@
-table = {
-    '0001101': 0,
-    '0011001': 1,
-    '0010011': 2,
-    '0111101': 3,
-    '0100011': 4,
-    '0110001': 5,
-    '0101111': 6,
-    '0111011': 7,
-    '0110111': 8,
-    '0001011': 9,
-}
+table = {'0001101': 0, '0011001': 1, '0010011': 2, '0111101': 3, '0100011': 4, '0110001': 5, '0101111': 6, '0111011': 7, '0110111': 8, '0001011': 9}
 for t in range(1, int(input()) + 1):
     n, m = map(int, input().split())
     for _ in range(n):
-        line = input()
-        if int(line) != 0:
-            real_line = int(line)
-    line = str(int(str(int(real_line))[::-1]))[::-1]
+        line = int(input())
+        if line != 0:
+            real_line = line
+    line = str(int(str(real_line)[::-1]))[::-1]
     line = ('0' * (7 - (len(line) % 7))) + line
-    code = ''
-    cnt, chk = 0, 0
-    result = 0
+    cnt, chk, result = 0, 0, 0
     for i in range(0, len(line), 7):
         cnt += 1
         num = table[line[i : i + 7]]
@@ -31,5 +18,4 @@ for t in range(1, int(input()) + 1):
         result += num
     if chk % 10 != 0:
         result = 0
-
     print(f'#{t} {result}')
