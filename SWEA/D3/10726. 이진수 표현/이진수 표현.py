ce25cdccffb9900efcba8d0adc8613dea161dd11
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-20 16:19:10 +0900

    [D3] Title: 이진수 표현, Time: 1,197 ms, Memory: 63,284 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.py
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.py
@@ -2,16 +2,10 @@ for t in range(1, int(input()) + 1):
     n, m = map(int, input().split())
 
     m = format(m, 'b')
-    cnt, result = 0, 'OFF'
-    if n <= len(m):
-        for i in m[::-1]:
-            if cnt == n:
-                break
-            if i == '0':
-                result = 'OFF'
-                break
-            else:
-                result = 'ON'
-            cnt += 1
-
+    result = 'ON'
+    if n > len(m):
+        result = 'OFF'
+    else:
+        if '0' in m[-n:]:
+            result = 'OFF'
     print(f'#{t} {result}')
