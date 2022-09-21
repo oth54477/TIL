e497255ef5164006f9e699a06f6267815d498a78
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-22 01:45:01 +0900

    [D5] Title: [S/W 문제해결 응용] 1일차 - 암호코드 스캔, Time: 466 ms, Memory: 64,580 KB -BaekjoonHub

---
+++ SWEA/D5/1242. ［S／W 문제해결 응용］ 1일차 － 암호코드 스캔/README.md
@@ -0,0 +1,11 @@
+# [D5] [S/W 문제해결 응용] 1일차 - 암호코드 스캔 - 1242 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV15JEKKAM8CFAYD) 
+
+### 성능 요약
+
+메모리: 64,580 KB, 시간: 466 ms, 코드길이: 1,547 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D5/1242. ［S／W 문제해결 응용］ 1일차 － 암호코드 스캔/［S／W 문제해결 응용］ 1일차 － 암호코드 스캔.py
@@ -0,0 +1,65 @@
+table = {
+    "211": "0",
+    "221": "1",
+    "122": "2",
+    "411": "3",
+    "132": "4",
+    "231": "5",
+    "114": "6",
+    "312": "7",
+    "213": "8",
+    "112": "9",
+}
+
+
+def find_ratio():
+    global binary
+    i_0 = binary.index('0')
+    binary = binary[i_0:]
+    i_1 = binary.index('1')
+    binary = binary[i_1:]
+    if '0' in binary:
+        i_00 = binary.index('0')
+    else:
+        i_00 = len(binary)
+    ratio = [i_0, i_1, i_00]
+    binary = binary[i_00:]
+    return ratio
+
+
+for t in range(1, int(input()) + 1):
+    n, m = map(int, input().split())
+    lines = set()
+    for _ in range(n):
+        line = input()[:m].strip()
+        if not line.isnumeric():
+            lines.add(line.strip('0'))
+
+    codes = set()
+    for line in lines:
+        binary = format(int(line, 16), 'b')
+        binary = binary.strip('0')
+        code = ''
+        while binary:
+            ratio = find_ratio()
+            min_num = min(ratio)
+            a, b, c = map(lambda x: str(x//min_num), ratio)
+            ratio = a+b+c
+            code += table[ratio]
+            binary = binary.strip('0')
+            if len(code) == 8:
+                codes.add(code)
+                code = ''
+    result = 0
+    for code in codes:
+        chk_num, sum_num = 0, 0
+        for idx, num in enumerate(code):
+            num = int(num)
+            if idx % 2 == 0:
+                chk_num += num * 3
+            else:
+                chk_num += num
+            sum_num += num
+        if chk_num % 10 == 0:
+            result += sum_num
+    print(f'#{t} {result}')
