84efb4c4ed3a8999f59b70d232e4ad64835f955c
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-23 16:45:39 +0900

    [Silver IV] Title: 트리 만들기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14244. 트리 만들기/트리 만들기.py
+++ 백준/Silver/14244. 트리 만들기/트리 만들기.py
@@ -2,9 +2,9 @@ n,m = map(int, input().split())
 
 nums = list(range(n))
 
-num = nums[:-(m-1)]
-nn = nums[-(m-1):]
-for i in range(len(num)-1):
-    print(num[i], num[i+1])
-for j in nn:
-    print(num[-1], j)
+nodes = nums[:-(m-1)]
+lead = nums[-(m-1):]
+for i in range(len(nodes)-1):
+    print(nodes[i], nodes[i+1])
+for j in lead:
+    print(nodes[-1], j)
