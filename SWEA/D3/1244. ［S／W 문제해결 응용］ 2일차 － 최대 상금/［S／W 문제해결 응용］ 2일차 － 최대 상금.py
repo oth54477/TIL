a2a4ab59b321de4d7dd3a16500966ac4d76f1d19
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-23 10:32:58 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 232 ms, Memory: 62,532 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,22 +1,25 @@
 from itertools import permutations
 
-def change(arr, m):
-    if m == n:
-        result.add(int(''.join(list(map(str,arr)))))
-        return 
-    for case in permutations(list(range(len(arr))), 2):
-        a = list(arr)
-        a[case[0]], a[case[1]] = a[case[1]], a[case[0]] 
-        b = int(''.join(list(map(str,a))))
-        if b not in tmp[m]:
-            tmp[m].append(b)
-            change(a, m+1)
+
+def change(nums, m):
+    if m == n:                                              # 최대 교환 횟수만큼 교환하면
+        result.add(int(''.join(list(map(str,nums)))))       # result에 int형으로 추가하기
+        return                                              # 재귀 탈출
+    for case in permutations(list(range(len(nums))), 2):    # 요소 개수가 2인 순열 생성
+        copy_nums = list(nums)                              # 리스트 카피
+        copy_nums[case[0]], copy_nums[case[1]] = copy_nums[case[1]], copy_nums[case[0]] # 카드 바꿔주기
+        int_cards = int(''.join(list(map(str,copy_nums))))  # int형으로 바꾸기
+        if int_cards not in tmp[m]:                         # 중복 제거
+            tmp[m].add(int_cards)                        # 중복 확인을 위해 리스트에 추가
+            change(copy_nums, m+1)                          # 재귀
         
+
 for t in range(1, int(input()) + 1):
-    info, n = map(str, input().split())
+    nums, n = map(str, input().split())
     n = int(n)
-    info = list(map(int, info))
+    nums = list(map(int, nums))
     result = set()
-    tmp = [[] for _ in range(10)]
-    change(info, 0)
+    # tmp = [[] for _ in range(10)]
+    tmp = [set() for _ in range(10)]
+    change(nums, 0)
     print(f'#{t} {max(result)}')
