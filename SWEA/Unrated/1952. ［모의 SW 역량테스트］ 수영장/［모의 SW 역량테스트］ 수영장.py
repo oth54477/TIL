37290f5d06aa486f6195e32b98309f69afa32085
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-25 17:18:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 140 ms, Memory: 58,736 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
@@ -1,17 +1,15 @@
-def day_or_month(month_idx):
-    day_cost = plan[month_idx] * prices[0]
-    month_cost = prices[1]
-    return min(day_cost, month_cost)
-
-
 for t in range(1, int(input()) + 1):
     prices = list(map(int, input().split()))
     plan = list(map(int, input().split()))
     total_cost = [0] * 13
     for i in range(1, 13):
-        total_cost[i] = total_cost[i - 1] + day_or_month(i - 1)
+        day_cost = plan[i - 1] * prices[0]
+        month_cost = prices[1]
+        total_cost[i] = total_cost[i - 1] + min(day_cost, month_cost)
         if i >= 3:
-            month_3 = total_cost[i - 3] + prices[2]
+            month_3 = (
+                total_cost[i - 3] + prices[2]
+            )  # 1일권 / 1달권 비교한 누적값 , 3개월전 누적값  + 3개월권
             total_cost[i] = min(total_cost[i], month_3)
     year_or_total = min(prices[3], total_cost[12])
     print(f'#{t} {year_or_total}')
