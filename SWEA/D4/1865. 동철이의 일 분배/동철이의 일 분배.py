520f5f755011fc2abba6735497e61cd268887802
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-27 16:20:01 +0900

    [D4] Title: 동철이의 일 분배, Time: 4,966 ms, Memory: 68,656 KB -BaekjoonHub

--- SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.py
+++ SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.py
@@ -1,23 +1,20 @@
-def success_rate(person, r, visit):
+def success_rate(person, rate, visit):
     global result
     if person == n:
-        result = max(result, r)
+        result = max(result, rate)
         return
-    if r <= result:
+    if rate <= result:
         return
     for i in range(n):
         if not visit[i]:
             visit[i] = True
-            success_rate(person + 1, r * rate[person][i], visit)
+            success_rate(person + 1, rate * rate_arr[person][i], visit)
             visit[i] = False
-
-
 
 for t in range(1, int(input()) + 1):
     n = int(input())
-    rate = [list(map(lambda x: int(x) / 100, input().split())) for _ in range(n)]
+    rate_arr = [list(map(lambda x: int(x) / 100, input().split())) for _ in range(n)]
     visited = [False] * n
     result = 0
     success_rate(0, 1, visited)
-    max_rate = round(result * 100, 6)  
-    print(f'#{t} {max_rate:.6f}')
+    print(f'#{t} {round(result * 100, 6)  :.6f}')
