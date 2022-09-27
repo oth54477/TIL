9bb0611ea3c68821b1df2f636392f7949778c097
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-28 01:04:48 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 119 ms, Memory: 56,740 KB -BaekjoonHub

---
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -0,0 +1,21 @@
+def chk_reverse(s):
+    global cnt
+    if s == s[::-1]:
+        cnt += 1
+
+for t in range(1, 11):
+    n = int(input())
+    board = []
+    cnt = 0
+    for _ in range(8):
+        strings = input()
+        board.append(list(map(str, strings)))
+        for idx in range(8 - n + 1):
+            chk = strings[0+idx:n+idx]
+            chk_reverse(chk)
+    reverse_board = list(map(list, zip(*board)))
+    for line in reverse_board:
+        for idx in range(8 - n + 1):
+            chk = line[0 + idx:n + idx]
+            chk_reverse(chk)
+    print(f'#{t} {cnt}')
