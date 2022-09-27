1469c954f9ffc080affb3469d85bba1eceb4d196
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-27 16:24:10 +0900

    [D4] Title: 동철이의 일 분배, Time: 5,671 ms, Memory: 68,392 KB -BaekjoonHub

--- SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.py
+++ SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.py
@@ -1,20 +1,22 @@
-def success_rate(person, rate, visit):
-    global result
-    if person == n:
-        result = max(result, rate)
+def success_rate(person, rate):
+    global max_rate
+    if person == n:                                                                     # 전부 계산하면
+        max_rate = max(max_rate, rate)                                                  # 더 큰값 저장
         return
-    if rate <= result:
+    if rate <= max_rate:                                                                # 백트래킹
         return
-    for i in range(n):
-        if not visit[i]:
-            visit[i] = True
-            success_rate(person + 1, rate * rate_arr[person][i], visit)
-            visit[i] = False
+    for i in range(n):                                                                  # 모든 경우의 수 탐색
+        if not visited[i]:                                                              # 아직 방문하지 않았으면
+            visited[i] = True                                                           # 방문 처리
+            success_rate(person + 1, rate * rate_arr[person][i])                        # 재귀 (다음사람, 비율, 방문)
+            visited[i] = False                                                          # 방문 처리 해제
+
+
 
 for t in range(1, int(input()) + 1):
     n = int(input())
-    rate_arr = [list(map(lambda x: int(x) / 100, input().split())) for _ in range(n)]
-    visited = [False] * n
-    result = 0
-    success_rate(0, 1, visited)
-    print(f'#{t} {round(result * 100, 6)  :.6f}')
+    rate_arr = [list(map(lambda x: int(x) / 100, input().split())) for _ in range(n)]   # 100을 나눠서 리스트에 저장
+    visited = [False] * n                                                               # 방문 리스트 생성
+    max_rate = 0                                                                        # 최대값 저장할 변수
+    success_rate(0, 1)                                                                  # 성공률 찾기
+    print(f'#{t} {round(max_rate * 100, 6)  :.6f}')                                     # 소수점 6자리까지
