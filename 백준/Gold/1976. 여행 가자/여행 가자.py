8b29775321a3b55dd63cb5058540b887fc5aed01
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-09-29 10:29:13 +0900

    [Gold IV] Title: 여행 가자, Time: 92 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -0,0 +1,26 @@
+def find_set(node):
+    if node != parent[node]:
+        parent[node] = find_set(parent[node])
+    return parent[node]
+
+
+n = int(input())
+m = int(input())
+parent = list(range(n))
+for i in range(n):
+    line = list(map(int, input().split()))
+    for j in range(n):
+        x, y = i, j
+        if line[j]:
+            x_root, y_root = find_set(x), find_set(y)  # Find
+            # Union
+            if x_root != y_root:  # 서로소 집합인 경우만 합집합 연산
+                if x_root < y_root:
+                    parent[y_root] = x_root
+                else:
+                    parent[x_root] = y_root
+plan = set(map(lambda x: parent[int(x) - 1], input().split()))
+if len(plan) == 1:
+    print('YES')
+else:
+    print('NO')
