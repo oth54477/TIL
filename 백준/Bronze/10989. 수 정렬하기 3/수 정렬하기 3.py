56e82ab54f3a73b78f1f4ad503095ce80be53b82
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2022-10-24 13:45:33 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8760 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,9 +1,11 @@
 import sys
+
 input = sys.stdin.readline
+
+n = int(input())
 arr = [0] * 10001
-for _ in range(int(input())):
+for _ in range(n):
     arr[int(input())] += 1
 for i, v in enumerate(arr):
-    if v:
-        for _ in range(v):
-            print(i)
+    for _ in range(v):
+        print(i)
