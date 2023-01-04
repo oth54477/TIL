c60f0a507640c6e5956317e075e38ce0bfd05c51
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2023-01-05 00:32:52 +0900

    [Silver III] Title: N과 M (4), Time: 60 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(arr):
+    global n, m
+    if len(arr) == m:
+        print(*arr)
+        return
+
+    for i in range(1, n+1):
+        if arr:
+            if arr[-1] > i:
+                continue
+        dfs(arr + [i])
+
+n, m = map(int, input().split())
+dfs([])
