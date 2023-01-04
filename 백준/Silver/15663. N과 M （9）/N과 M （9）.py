343912b4a9519c0f93cb4d3dd05396aa7e1be808
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2023-01-05 00:52:07 +0900

    [Silver II] Title: N과 M (9), Time: 128 ms, Memory: 35080 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+def dfs(arr,numbers):
+    global n, m
+    if len(arr) == m:
+        if arr not in result:
+            print(*arr)
+        result.add(arr)
+        return
+    for idx, i in enumerate(numbers):
+        dfs(arr + (i,), numbers[:idx] + numbers[idx+1:])
+n, m = map(int, input().split())
+numbers = sorted(list(map(int, input().split())))
+result = set()
+dfs((),numbers)
