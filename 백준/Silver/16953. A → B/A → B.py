e7545cf2d623c07650c34aa8a7c9bb5d27a65b23
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2023-01-18 16:29:12 +0900

    [Silver II] Title: A → B, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,32 +1,26 @@
 import sys
-from collections import deque
-
 input = sys.stdin.readline
-
-def case1(x):
-    return x*2
-
-def case2(x):
-    return x*10 + 1
-
-a, b = map(int ,input().strip().split())
-dq = deque([(a,1)])
-result = -1
+a, b = map(int, input().strip().split())
 cnt = 1
-while  dq:
-    x, cnt = dq.popleft()
-    x1 = case1(x)
-    if x1 < b:
-        dq.append((case1(x), cnt+1))
-    elif x1 == b:
-        result = cnt
+while True:
+    s = str(b)
+    if s[-1] == '1':
+        if s == '1':
+            result = -1
+            break
+        b = int(s[:-1])
+        cnt += 1
+        if a == b:
+            result = cnt
+            break
+    elif b % 2 == 0:
+        b //= 2
+        cnt += 1
+        if a == b:
+            result = cnt
+            break
+    else:
+        result = -1
         break
-    x2 = case2(x)
-    if x2 < b:
-        dq.append((case2(x), cnt + 1))
-    elif x2 == b:
-        result = cnt
-        break
-else:
-    result = -2
-print(result + 1)
+    
+print(result)
