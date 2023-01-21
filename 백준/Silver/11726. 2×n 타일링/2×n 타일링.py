3776c5c76254215bd761697c9ef303706430c941
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2023-01-21 20:19:40 +0900

    [Silver III] Title: 2×n 타일링, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -2,14 +2,12 @@ import sys
 input = sys.stdin.readline
 
 n = int(input().strip())
-
 dp = [0,1,2] 
-
 if n <= 2:
     print(dp[n])
 else:
-    dp += [0] * (n-2)
+    now = 2
+    old = 1
     for i in range(3, n+1):
-        dp[i] = dp[i-1] + dp[i-2]
-    print(dp[n]%10007)
-
+        now, old = now + old, now
+    print(now%10007)
