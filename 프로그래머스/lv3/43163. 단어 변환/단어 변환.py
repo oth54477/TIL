bfbf0049024cba1efff64b111315023c6219764f
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2023-02-21 00:37:13 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -3,22 +3,29 @@ from collections import deque
 def solution(begin, target, words):
     if target not in words:
         return 0
-    L = len(words)
-    l = len(begin)
-    visited = [False] * L
+    
+    # 방문한 단어를 저장하는 리스트 대신, 집합으로 변경
+    visited = set()
     dq = deque([(begin, 0)])
+    
     while dq:
         word, count = dq.popleft()
-        for idx, next_word in enumerate(words):
-            if not visited[idx]:
-                cnt = 0
-                for i in range(l):
-                    if word[i] != next_word[i]:
-                        cnt += 1
-                    if cnt > 1:
-                        break
-                else:
-                    if next_word == target:
-                        return count + 1
-                    dq.append((next_word, count + 1))
-                    visited[idx] = True
+        
+        for next_word in words:
+            if next_word not in visited and is_one_diff(word, next_word):
+                if next_word == target:
+                    return count + 1
+                dq.append((next_word, count + 1))
+                visited.add(next_word)
+    
+    return 0
+
+# 두 단어가 한 개의 알파벳만 다른지 확인하는 함수
+def is_one_diff(word1, word2):
+    diff_count = 0
+    for c1, c2 in zip(word1, word2):
+        if c1 != c2:
+            diff_count += 1
+            if diff_count > 1:
+                return False
+    return diff_count == 1
