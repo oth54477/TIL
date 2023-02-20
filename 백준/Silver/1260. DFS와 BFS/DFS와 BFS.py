bc3eb57b39c14c7165dd8dd874b3b586dc8a22a8
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2023-02-21 00:01:45 +0900

    [Silver II] Title: DFS와 BFS, Time: 84 ms, Memory: 34168 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,43 +1,43 @@
-# 1260. DFS와 BFS
-# https://www.acmicpc.net/problem/1260
-
 import sys
 from collections import deque
 
-def dfs(node):
-    visited[node] = True
-    dfs_arr.append(node)
-    for next_node in graph[node]:
-        if not visited[next_node]:
-            dfs(next_node)
-
-def bfs(node):
-    visited[node] = True
-    q = deque([node])
-    while q:
-        node = q.popleft()
-        bfs_arr.append(node)
-        for next_node in graph[node]:
-            if not visited[next_node]:
-                visited[next_node] = True
-                q.append(next_node)
-
-
 input = sys.stdin.readline
 
-n, m, v = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
-for _ in range(m):
-    s, e = map(int, input().split())
-    graph[s].append(e)
-    graph[e].append(s)
-for i in range(n+1):
-    graph[i].sort()
-dfs_arr, bfs_arr = [], []
-visited = [False] * (n + 1)
-dfs(v)
-visited = [False] * (n + 1)
-bfs(v)
-    
-print(*dfs_arr)
-print(*bfs_arr)
+def dfs(v):
+    visited[v] = True
+    result.append(v)
+    for next_v in graph[v]:
+        if not visited[next_v]:
+            dfs(next_v)
+
+def bfs(v):
+    visited = [True] + [False] * N
+    dq = deque([v])
+    visited[v] = True
+    while dq:
+        v = dq.popleft()
+        result.append(v)
+        for next_v in graph[v]:
+            if not visited[next_v]:
+                dq.append(next_v)
+                visited[next_v] = True
+
+N, M, V = map(int, input().strip().split())
+
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    v1, v2 = map(int, input().strip().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+graph = list(map(sorted, graph))
+
+visited = [True] + [False] * N
+result = []
+dfs(V)
+print(*result)
+
+result = []
+bfs(V)
+print(*result)
