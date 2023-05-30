1dd3334e737a618fb69bb16cbf34e992116f87ee
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2023-05-30 14:11:56 +0900

    [Silver I] Title: IOIOI, Time: 160 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+
+N = int(input().strip())
+M = int(input().strip())
+S = input().strip()
+
+p = "IO" * N + "I"
+
+point = N * 2 + 1
+
+count = 0
+
+s = S[:point]
+if s == p:
+    count += 1
+for i in range(point, M):
+    s = s[1:] + S[i]
+    if s == p:
+        count += 1
+print(count)
