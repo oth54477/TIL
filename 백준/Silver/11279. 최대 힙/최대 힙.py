97354953d8ea26983c0cb27a1d70a425c506c4a3
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2023-06-05 15:49:34 +0900

    [Silver II] Title: 최대 힙, Time: 212 ms, Memory: 118868 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,49 +1,16 @@
 import sys
-
-def heap_push(item):
-    heap.append(item)
-    child = len(heap) - 1
-    parent = child // 2
-
-    while parent > 0 and heap[parent] < heap[child]:
-        heap[parent], heap[child] = heap[child], heap[parent]
-        child = parent
-        parent = child // 2
-
-def heap_pop():
-    if len(heap) <= 2:
-        return heap.pop()
-
-    item = heap[1]
-    heap[1] = heap.pop()
-    parent = 1
-    child = parent * 2
-
-    while child < len(heap):
-        if child + 1 < len(heap) and heap[child] < heap[child+1]:
-            child+=1
-        if heap[parent] < heap[child]:
-            heap[parent], heap[child] = heap[child], heap[parent]
-            parent = child
-            child = parent * 2
-        else:
-            break
-    return item
-
-
-
-
+from heapq import heappush, heappop
 
 input = sys.stdin.readline
 
-heap = [0]
+heap = []
 n = int(input().strip())
 for _ in range(n):
     x = int(input().strip())
     if x != 0:
-        heap_push(x)
+        heappush(heap, -x)
     else:
-        if len(heap) == 1:
+        if len(heap) == 0:
             print(0)
         else:
-            print(heap_pop())
+            print(-heappop(heap))
