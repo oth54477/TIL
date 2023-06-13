646c5ee3a0dc058297b5e2c421e16e6ec120bdbc
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2023-06-13 21:41:13 +0900

    [Gold V] Title: 트리, Time: 64 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -1,53 +1,31 @@
 import sys
-from collections import deque
+from collections import deque, defaultdict
 
 input = sys.stdin.readline
-
-
-class Node:
-    def __init__(self, data):
-        self.parent = None
-        self.children = []
-        self.number = data
-
-
 N = int(input().strip())
 
 tree = list(map(int, input().strip().split()))
 
 M = int(input().strip())
 
-tree_dict = dict()
+graph = defaultdict(list)
 
-tree_dict[-1] = Node(-1)
-
-for i, p in enumerate(tree):
-    if i in tree_dict:
-        node = tree_dict[i]
-    else:
-        node = Node(i)
-    if p not in tree_dict:
-        tree_dict[p] = Node(p)
-    node.parent = tree_dict[p]
-    tree_dict[p].children.append(node)
-    tree_dict[i] = node
+for i, v in enumerate(tree):
+    graph[v].append(i)
 
 dq = deque([-1])
-
 count = 0
-
 while dq:
     n = dq.popleft()
-    if n != M:
-        if n in tree_dict and tree_dict[n].children:
-            for child in tree_dict[n].children:
-                if child.number != M:
-                    dq.append(child.number)
-                elif len(tree_dict[n].children) == 1:
-                    count += 1
-
-        else:
-            count += 1
+    nodes = graph[n]
+    if nodes:
+        for node in nodes:
+            if node != M:
+                dq.append(node)
+            elif len(nodes) == 1:
+                count += 1
+    else:
+        count += 1
 
 if n == -1:
     count = 0
