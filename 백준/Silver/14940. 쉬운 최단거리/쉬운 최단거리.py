b80637c3daaabdcd21015c5e2d617d4b131382c5
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2023-10-23 21:54:25 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 412 ms, Memory: 161544 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,26 +1,27 @@
 import sys
 from collections import deque
-from itertools import combinations
 
 input = sys.stdin.readline
 
 N, M = map(int, input().strip().split())
 arr = [list(map(int, input().strip().split())) for _ in range(N)]
+d = [(0,1), (0,-1), (1,0), (-1,0)]
 
+# 원래 갈 수 있는 땅인 부분 중에서 도달할 수 없는 위치를 찾기 위한 set
 check_set = set()
+visited = set()
+
+# 출발 위치를 찾고, 갈 수 있는 땅 기록
 for r, line in enumerate(arr):
         for c, value in enumerate(line):
             if value == 2:
-                start = (r, c)
+                visited.add((r, c))
+                dq = deque([(r, c, 1)])
+                arr[r][c] = 0
             elif value == 1:
-                 check_set.add((r, c))
+                check_set.add((r, c))
 
-visited = set()
-d = [(0,1), (0,-1), (1,0), (-1,0)]
-visited.add((start[0], start[1]))
-dq = deque([(start[0], start[1], 1)])
-arr[start[0]][start[1]] = 0
-
+# BFS
 while dq:
     r, c, cnt = dq.popleft()
     for dr, dc in d:
@@ -30,6 +31,7 @@ while dq:
             arr[nr][nc] = cnt
             dq.append((nr, nc, cnt + 1))
 
+# 원래 갈 수 있는 땅인 부분 중에서 도달할 수 없는 위치
 remain = check_set - visited
 
 for r, c in remain:
