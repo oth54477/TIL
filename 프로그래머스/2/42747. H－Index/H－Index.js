d6e2125a01f894fe91d78770f5463ada9ccf58ed
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2025-06-26 01:52:50 +0900

    [level 2] Title: H-Index, Time: 39.13 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,12 +1,7 @@
 function solution(citations) {
-    const sortedArr =  citations.sort((a, b) => b - a)
-    
-    for (let h=0; h < sortedArr.length; h++) {
-        const nowC = sortedArr[h]
-        
-        if (nowC < h+1) return h
-    }
-    
-    const lastC = sortedArr[sortedArr.length - 1]
-    return lastC > sortedArr.length ? sortedArr.length : lastC === 0 ? 0 : 1
+    let i = 0;
+
+    while(i + 1 <= citations.sort((a, b) => b - a)[i]) i++;
+
+    return i;
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.44 ms
+메모리: 36.4 MB, 시간: 39.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 26일 01:51:34
+2025년 06월 26일 01:52:48
 
 ### 문제 설명
 
