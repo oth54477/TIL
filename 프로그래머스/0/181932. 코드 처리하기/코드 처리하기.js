76927c85adcb04cca2063dfe15aacd63736c3bbc
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2025-08-05 09:54:04 +0900

    [level 0] Title: 코드 처리하기, Time: 27.51 ms, Memory: 47 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -1,20 +1,34 @@
 function solution(code) {
+    const toggleMode = () => {
+        mode = mode === 0 ? 1 : 0;
+    }
+
+    const checkIsEven = (index) => {
+        return index % 2 === 0;
+    }
+
+    const checkCondition = (mode, index) => {
+        if (mode === 0 && checkIsEven(index)) {
+            return true;
+        }
+
+        if (mode === 1 && !checkIsEven(index)) {
+            return true;
+        }
+    }
+
     let mode = 0;
     let ret = '';
-    
+
     for (let index in code) {
         const current = code[index];
 
         if (current === '1') {
-            mode = mode === 0 ? 1 : 0;
+            toggleMode();
             continue;
         }
 
-        if (mode === 0 && index % 2 === 0) {
-            ret += current;
-        }
-        
-        if (mode === 1 && index % 2 === 1) {
+        if (checkCondition(mode, index)) {
             ret += current;
         }
     }
