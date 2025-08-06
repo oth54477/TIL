2a151e78767d0dcb57f3f70d6b5a63033067c1e7
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2025-08-06 10:58:35 +0900

    [level 2] Title: 전화번호 목록, Time: 132.14 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,11 +1,1 @@
-function solution(phoneBook) {
-    phoneBook.sort((a,b) => a.length - b.length)
-    return !(new Array(20).fill(0).some((_, index) => {
-        return phoneBook.some((num) => num.length === index) && (phoneBook.reduce((acc, num) => {
-            const temp = num.slice(0, index)
-            !acc[1].has(temp) && num.length !== index ? acc[0].add(num) : acc[0].add(temp)
-            if (num.length === index) acc[1].add(temp)
-            return acc
-        }, [new Set(), new Set()])[0]).size !== phoneBook.length;
-    }))
-}
+const solution = (phoneBook) => !phoneBook.sort().some((num, i) => phoneBook[i + 1]?.startsWith(num))
