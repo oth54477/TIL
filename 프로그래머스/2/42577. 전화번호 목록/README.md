4639b55ff7458e20dae79ec231ff584cd8cb6016
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2025-08-06 10:58:04 +0900

    [level 2] Title: 전화번호 목록, Time: 148.26 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=javascript#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 79.6 MB, 시간: 132.14 ms
+메모리: 87.5 MB, 시간: 148.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 06일 10:49:10
+2025년 08월 06일 10:58:01
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,1 +1,11 @@
-const solution = (phoneBook) => !phoneBook.sort().some((num, i) => phoneBook[i + 1]?.startsWith(num))
+function solution(phoneBook) {
+    phoneBook.sort((a,b) => a.length - b.length)
+    return !(new Array(20).fill(0).some((_, index) => {
+        return phoneBook.some((num) => num.length === index) && (phoneBook.reduce((acc, num) => {
+            const temp = num.slice(0, index)
+            !acc[1].has(temp) && num.length !== index ? acc[0].add(num) : acc[0].add(temp)
+            if (num.length === index) acc[1].add(temp)
+            return acc
+        }, [new Set(), new Set()])[0]).size !== phoneBook.length;
+    }))
+}
