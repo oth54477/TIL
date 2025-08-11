1f0f033d4ee2a1f2216d697458198a0e341c5af1
Taehun Oh <66712534+oth54477@users.noreply.github.com>
2025-08-11 09:22:48 +0900

    [level 1] Title: 추억 점수, Time: 0.81 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,19 +1,27 @@
 function solution(name, yearning, photo) {
-    const pointObj = name.reduce((acc, cur, index) => {
-        acc[cur] = yearning[index];
-        
-        return acc;
-    }, {})
+    const makePointObjByName = (name, yearning) => {
+        return name.reduce((acc, cur, index) => {
+            acc[cur] = yearning[index];
+
+            return acc;
+        }, {})
+    }
     
-    return photo.reduce((acc, cur) => {
-        acc.push(cur.reduce((point, name) => {
+    const getPoint = (pointObj, photoItem) => {
+        return photoItem.reduce((point, name) => {
             const currentPoint = pointObj[name];
             if (currentPoint) {
                 point += pointObj[name];    
             }
             
             return point;
-        }, 0))
+        }, 0)
+    }
+    
+    const pointObj = makePointObjByName(name, yearning);
+    
+    return photo.reduce((acc, cur) => {
+        acc.push(getPoint(pointObj, cur))
         
         return acc;
     }, [])
